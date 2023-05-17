5bc5544daccef0105b1877125177edf18dc39729
KJS <otw1917@naver.com>
2023-05-17 12:18:48 +0900

    OAuth2.0 적용으로 인한 test 코드 오류를 해결

--- src/main/java/com/jojoldu/book/config/JpaConfig.java
+++ src/main/java/com/jojoldu/book/config/JpaConfig.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+    // test code에서 @SpringBootTest를 사용하지 않는 경우 오류가 발생함.
+    // JpaAuditiong은 별도의 config 파일에서 설정해야함.
 }
